a4a4aa68b34565081bbf3d535d45b990b884acbb
Navya-4 <73339744+Navya-4@users.noreply.github.com>
2020-12-19 22:34:56 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,35 @@
+class Box  {
+  constructor(x, y, width, height){
+
+    var options = {
+      
+      'restitution':1,
+      'friction':0.5,
+      'density':0.5,
+      isStatic: false
+  }
+
+    this.body = Bodies.rectangle(x,y,width,height,options)
+    this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+
+      
+  }
+  
+display(){
+var pos = this.body.position;
+  push()
+      translate(pos.x,pos.y)
+      rotate(this.body.angle)
+      strokeWeight ("4")
+      stroke ("lightgreen")
+      fill ("cyan")
+      rectMode(CENTER);
+      rect (0, 0 , this.width, this.height)
+    pop();
+  
+
+
+}
+};
